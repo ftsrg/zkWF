70052a07aa6f0eb91a5a909091a32e11df61d7f6
Balazs Toldi <balazs@toldi.eu>
2024-11-10 09:29:22 +0100

    chore: refactor statechecker circuit
    Signed-off-by: Balazs Toldi <balazs@toldi.eu>

--- pkg/circuits/statechecker/circuit.go
+++ pkg/circuits/statechecker/circuit.go
@@ -67,40 +67,8 @@ func (circuit Circuit) Define(api frontend.API) error {
 	// Randomness check
 	api.AssertIsDifferent(circuit.State_curr.Radomness, circuit.State_new.Radomness)
 
-	same := make([]frontend.Variable, N)
-	complated := make([]frontend.Variable, N)
-	stateChanged := make([]frontend.Variable, N) // State changed from curr to new, but not completed
-	for i, curr := range circuit.State_curr.States {
-		new := circuit.State_new.States[i]
-		same[i] = utils.IsEqual(api, curr, new)
-		complated[i] = api.Select(same[i], common.FALSE, utils.IsEqual(api, new, common.STATE_COMPLETED))
-
-		lifecycleCheck := lifecycle.LifecycleCheck(api, curr, new)
-		api.AssertIsEqual(api.Select(same[i], common.TRUE, lifecycleCheck), common.TRUE)
-
-		stateChanged[i] = api.Select(same[i], common.FALSE, api.Select(complated[i], common.FALSE, common.TRUE))
-	}
-
-	comp_sum := make([]frontend.Variable, N)
-	diff_sum := make([]frontend.Variable, N)
-
-	comp_sum[0] = complated[0]
-	diff_sum[0] = utils.Not(api, same[0])
-	for i := 1; i < N; i++ {
-		comp_sum[i] = api.Add(comp_sum[i-1], complated[i])
-		diff_sum[i] = api.Add(diff_sum[i-1], utils.Not(api, same[i]))
-	}
-	api.Println("comp_sum: ", comp_sum[N-1])
-	api.Println("diff_sum: ", diff_sum[N-1])
-	noChange := utils.IsEqual(api, diff_sum[N-1], 0)
-	api.Println("No change: ", noChange)
-	isActivated := utils.IsEqual(api, comp_sum[N-1], 1)
-	api.Println("Is activated: ", isActivated)
-	justOne := api.Select(isActivated, 0, utils.IsEqual(api, diff_sum[N-1], 1))
-	api.Println("Just one: ", justOne)
-	validChanges := api.Select(noChange, common.TRUE, api.Xor(isActivated, justOne))
-
-	api.AssertIsEqual(validChanges, common.TRUE)
+	// State changed from curr to new, but not completed
+	same, complated, stateChanged := stage1(N, circuit, api)
 
 	readyInTheRightTime := make([]frontend.Variable, N)
 	completedAndTokenForwarded := make([]frontend.Variable, N)
@@ -110,111 +78,7 @@ func (circuit Circuit) Define(api frontend.API) error {
 		involvedWithPayment[i] = common.FALSE
 	}
 
-	for _, task := range executables {
-		index := indexOf(executables, task)
-		incomingNode := task.Incoming[0].SourceRef
-		gotReady := api.And(api.Select(utils.IsEqual(api, circuit.State_new.States[index], common.STATE_READY), common.TRUE, common.FALSE), stateChanged[index])
-		changed := utils.Not(api, same[index])
-		pubKeySameX := utils.IsEqual(api, circuit.Keys.PublicKey.A.X, task.Owner.PublicKey[0])
-		pubKeySameY := utils.IsEqual(api, circuit.Keys.PublicKey.A.Y, task.Owner.PublicKey[1])
-		pubKeySame := api.And(pubKeySameX, pubKeySameY)
-		api.AssertIsEqual(api.Select(changed, pubKeySame, common.TRUE), common.TRUE)
-
-		// Stuff got completed
-		switch task.Type {
-		case model.INTERMEDIATE_THROW_EVENT:
-			messageId := circuit.Model.MessageMap[task.ID]
-			messageSame := utils.IsEqual(api, circuit.State_curr.Messages[messageId], circuit.State_new.Messages[messageId])
-			messageUpdated := utils.Not(api, messageSame)
-			api.AssertIsEqual(api.Select(complated[index], messageUpdated, common.TRUE), common.TRUE)
-		case model.INTERMEDIATE_CATCH_EVENT:
-			// TODO: Check if the message is sent by the THROW event (check if the event is completed)
-		case model.PAYMENT_TASK:
-			newBalance := circuit.State_new.Balances[task.Payment.Receiver]
-			expectedBalance := api.Add(circuit.State_curr.Balances[task.Payment.Receiver], task.Payment.Amount)
-			api.AssertIsEqual(api.Select(complated[index], utils.IsEqual(api, newBalance, expectedBalance), common.TRUE), common.TRUE)
-			newBalance = circuit.State_new.Balances[task.Owner.ID]
-			expectedBalance = api.Sub(circuit.State_curr.Balances[task.Owner.ID], task.Payment.Amount)
-			api.AssertIsEqual(api.Select(complated[index], utils.IsEqual(api, newBalance, expectedBalance), common.TRUE), common.TRUE)
-			involvedWithPayment[task.Owner.ID] = api.Select(complated[index], common.TRUE, involvedWithPayment[task.Owner.ID])
-			involvedWithPayment[task.Payment.Receiver] = api.Select(complated[index], common.TRUE, involvedWithPayment[task.Payment.Receiver])
-		}
-
-		switch incomingNode.Type {
-		case model.START_EVENT:
-			readyInTheRightTime[index] = 0
-		case model.PARALLEL_GATEWAY:
-			/*
-				In case of a ParallelGateway, the task is ready if all incoming nodes are completed
-			*/
-			incomingNodesCompleted := allPreviousNodesCompleted(api, circuit, task)
-			readyInTheRightTime[index] = api.And(gotReady, incomingNodesCompleted)
-		case model.EXCLUSIVE_GATEWAY, model.INCLUSIVE_GATEWAY:
-			/*
-				In case of an ExclusiveGateway, the task is ready if the incoming node (one) is completed
-			*/
-			incomingNodesOneCompleted := atLeastOnePreviousNodeCompleted(api, circuit, incomingNode)
-			readyInTheRightTime[index] = api.And(gotReady, incomingNodesOneCompleted)
-		default:
-			incomingNodeIndex := indexOf(executables, incomingNode)
-
-			readyInTheRightTime[index] = api.And(gotReady, complated[incomingNodeIndex])
-		}
-
-		outNode := task.Outgoing[0].TargetRef
-		switch outNode.Type {
-		case model.END_EVENT:
-			completedAndTokenForwarded[index] = complated[index]
-		case model.PARALLEL_GATEWAY:
-			/*
-				In case of a ParallelGateway, the task is completed if all outgoing nodes are ready when all the "paired" tasks are completed
-				If one pair is not completed, the task is completed but without activating the next task
-			*/
-
-			pairs := task.GetPairs()
-			pairsCompleted := make([]frontend.Variable, len(pairs))
-			for i, pair := range pairs {
-				pairIndex := indexOf(executables, pair)
-				if i == 0 {
-					pairsCompleted[i] = utils.IsEqual(api, circuit.State_new.States[pairIndex], common.STATE_COMPLETED)
-				} else {
-					pairsCompleted[i] = api.And(pairsCompleted[i-1], utils.IsEqual(api, circuit.State_new.States[pairIndex], common.STATE_COMPLETED))
-				}
-			}
-
-			outgoingNodesGotReady := parallelGatewayActivated(api, circuit, same, outNode)
-
-			/*completedAndTokenForwarded[index] = api.Select(complated[index], api.Select(pairsCompleted[len(pairs)-1], outgoingNodesGotReady[len(outgoingNodes)-1], utils.Not(api, outgoingNodesGotReady[len(outgoingNodes)-1])), common.FALSE)*/
-			if len(pairs) > 0 {
-				completedAndTokenForwarded[index] = api.Select(complated[index], api.Select(pairsCompleted[len(pairs)-1], outgoingNodesGotReady, utils.Not(api, outgoingNodesGotReady)), common.FALSE)
-				api.Println("Task: ", task.ID, " Completed: ", complated[index], " Pairs completed: ", pairsCompleted[len(pairs)-1], " Outgoing nodes ready: ", outgoingNodesGotReady, " Completed and token forwarded: ", completedAndTokenForwarded[index])
-			} else {
-				completedAndTokenForwarded[index] = api.Select(complated[index], outgoingNodesGotReady, common.FALSE)
-			}
-		case model.EXCLUSIVE_GATEWAY:
-			/*
-				In case of an ExclusiveGateway, the task is completed if one of the outgoing nodes is ready when the incoming node is completed AND
-				the expression on that edge is evaluated to true
-			*/
-			outgoingNodesGotReady := exclusiveGatewayActivated(api, circuit, same, outNode)
-			completedAndTokenForwarded[index] = api.Select(complated[index], outgoingNodesGotReady, common.FALSE)
-		case model.INCLUSIVE_GATEWAY:
-			outgoingNodesGotReady := inclusiveGatewayActivated(api, circuit, same, outNode)
-			completedAndTokenForwarded[index] = api.Select(complated[index], outgoingNodesGotReady, common.FALSE)
-		default:
-			outNodeIndex := indexOf(executables, outNode)
-			gotReady := api.And(api.Select(utils.IsEqual(api, circuit.State_new.States[outNodeIndex], common.STATE_READY), common.TRUE, common.FALSE), stateChanged[outNodeIndex])
-			completedAndTokenForwarded[index] = api.And(gotReady, complated[index])
-		}
-
-		// One and only one of the following conditions must be 1 (true): readyInTheRightTime, completedAndTokenForwarded, stateChanged, same
-		api.Println("Task: ", task.ID, " Ready: ", readyInTheRightTime[index], " Completed: ", completedAndTokenForwarded[index], " State changed: ", stateChanged[index], "Same: ", same[index])
-
-		readyOrStateChange := api.Xor(readyInTheRightTime[index], api.And(stateChanged[index], utils.Not(api, gotReady)))
-		complatedOrSame := api.Xor(completedAndTokenForwarded[index], same[index])
-		api.Println("Task: ", task.ID, " Final: ", api.Xor(readyOrStateChange, complatedOrSame))
-		api.AssertIsEqual(api.Xor(readyOrStateChange, complatedOrSame), common.TRUE)
-	}
+	stage2(executables, api, circuit, stateChanged, same, complated, involvedWithPayment, readyInTheRightTime, completedAndTokenForwarded)
 
 	checkPayments(api, circuit, involvedWithPayment)
 
@@ -273,6 +137,148 @@ func (circuit Circuit) Define(api frontend.API) error {
 	}
 
 	return nil
+}
+
+func stage2(executables []*model.Node, api frontend.API, circuit Circuit, stateChanged []frontend.Variable, same []frontend.Variable, complated []frontend.Variable, involvedWithPayment []frontend.Variable, readyInTheRightTime []frontend.Variable, completedAndTokenForwarded []frontend.Variable) {
+	for _, task := range executables {
+		index := indexOf(executables, task)
+		incomingNode := task.Incoming[0].SourceRef
+		gotReady := api.And(api.Select(utils.IsEqual(api, circuit.State_new.States[index], common.STATE_READY), common.TRUE, common.FALSE), stateChanged[index])
+		changed := utils.Not(api, same[index])
+		pubKeySameX := utils.IsEqual(api, circuit.Keys.PublicKey.A.X, task.Owner.PublicKey[0])
+		pubKeySameY := utils.IsEqual(api, circuit.Keys.PublicKey.A.Y, task.Owner.PublicKey[1])
+		pubKeySame := api.And(pubKeySameX, pubKeySameY)
+		api.AssertIsEqual(api.Select(changed, pubKeySame, common.TRUE), common.TRUE)
+
+		switch task.Type {
+		case model.INTERMEDIATE_THROW_EVENT:
+			messageId := circuit.Model.MessageMap[task.ID]
+			messageSame := utils.IsEqual(api, circuit.State_curr.Messages[messageId], circuit.State_new.Messages[messageId])
+			messageUpdated := utils.Not(api, messageSame)
+			api.AssertIsEqual(api.Select(complated[index], messageUpdated, common.TRUE), common.TRUE)
+		case model.INTERMEDIATE_CATCH_EVENT:
+			// TODO: Check if the message is sent by the THROW event (check if the event is completed)
+		case model.PAYMENT_TASK:
+			newBalance := circuit.State_new.Balances[task.Payment.Receiver]
+			expectedBalance := api.Add(circuit.State_curr.Balances[task.Payment.Receiver], task.Payment.Amount)
+			api.AssertIsEqual(api.Select(complated[index], utils.IsEqual(api, newBalance, expectedBalance), common.TRUE), common.TRUE)
+			newBalance = circuit.State_new.Balances[task.Owner.ID]
+			expectedBalance = api.Sub(circuit.State_curr.Balances[task.Owner.ID], task.Payment.Amount)
+			api.AssertIsEqual(api.Select(complated[index], utils.IsEqual(api, newBalance, expectedBalance), common.TRUE), common.TRUE)
+			involvedWithPayment[task.Owner.ID] = api.Select(complated[index], common.TRUE, involvedWithPayment[task.Owner.ID])
+			involvedWithPayment[task.Payment.Receiver] = api.Select(complated[index], common.TRUE, involvedWithPayment[task.Payment.Receiver])
+		}
+
+		switch incomingNode.Type {
+		case model.START_EVENT:
+			readyInTheRightTime[index] = 0
+		case model.PARALLEL_GATEWAY:
+			/*
+				In case of a ParallelGateway, the task is ready if all incoming nodes are completed
+			*/
+			incomingNodesCompleted := allPreviousNodesCompleted(api, circuit, task)
+			readyInTheRightTime[index] = api.And(gotReady, incomingNodesCompleted)
+		case model.EXCLUSIVE_GATEWAY, model.INCLUSIVE_GATEWAY:
+			/*
+				In case of an ExclusiveGateway, the task is ready if the incoming node (one) is completed
+			*/
+			incomingNodesOneCompleted := atLeastOnePreviousNodeCompleted(api, circuit, incomingNode)
+			readyInTheRightTime[index] = api.And(gotReady, incomingNodesOneCompleted)
+		default:
+			incomingNodeIndex := indexOf(executables, incomingNode)
+
+			readyInTheRightTime[index] = api.And(gotReady, complated[incomingNodeIndex])
+		}
+
+		outNode := task.Outgoing[0].TargetRef
+		switch outNode.Type {
+		case model.END_EVENT:
+			completedAndTokenForwarded[index] = complated[index]
+		case model.PARALLEL_GATEWAY:
+			/*
+				In case of a ParallelGateway, the task is completed if all outgoing nodes are ready when all the "paired" tasks are completed
+				If one pair is not completed, the task is completed but without activating the next task
+			*/
+			pairs := task.GetPairs()
+			pairsCompleted := make([]frontend.Variable, len(pairs))
+			for i, pair := range pairs {
+				pairIndex := indexOf(executables, pair)
+				if i == 0 {
+					pairsCompleted[i] = utils.IsEqual(api, circuit.State_new.States[pairIndex], common.STATE_COMPLETED)
+				} else {
+					pairsCompleted[i] = api.And(pairsCompleted[i-1], utils.IsEqual(api, circuit.State_new.States[pairIndex], common.STATE_COMPLETED))
+				}
+			}
+
+			outgoingNodesGotReady := parallelGatewayActivated(api, circuit, same, outNode)
+
+			if len(pairs) > 0 {
+				completedAndTokenForwarded[index] = api.Select(complated[index], api.Select(pairsCompleted[len(pairs)-1], outgoingNodesGotReady, utils.Not(api, outgoingNodesGotReady)), common.FALSE)
+				api.Println("Task: ", task.ID, " Completed: ", complated[index], " Pairs completed: ", pairsCompleted[len(pairs)-1], " Outgoing nodes ready: ", outgoingNodesGotReady, " Completed and token forwarded: ", completedAndTokenForwarded[index])
+			} else {
+				completedAndTokenForwarded[index] = api.Select(complated[index], outgoingNodesGotReady, common.FALSE)
+			}
+		case model.EXCLUSIVE_GATEWAY:
+			/*
+				In case of an ExclusiveGateway, the task is completed if one of the outgoing nodes is ready when the incoming node is completed AND
+				the expression on that edge is evaluated to true
+			*/
+			outgoingNodesGotReady := exclusiveGatewayActivated(api, circuit, same, outNode)
+			completedAndTokenForwarded[index] = api.Select(complated[index], outgoingNodesGotReady, common.FALSE)
+		case model.INCLUSIVE_GATEWAY:
+			outgoingNodesGotReady := inclusiveGatewayActivated(api, circuit, same, outNode)
+			completedAndTokenForwarded[index] = api.Select(complated[index], outgoingNodesGotReady, common.FALSE)
+		default:
+			outNodeIndex := indexOf(executables, outNode)
+			gotReady := api.And(api.Select(utils.IsEqual(api, circuit.State_new.States[outNodeIndex], common.STATE_READY), common.TRUE, common.FALSE), stateChanged[outNodeIndex])
+			completedAndTokenForwarded[index] = api.And(gotReady, complated[index])
+		}
+
+		api.Println("Task: ", task.ID, " Ready: ", readyInTheRightTime[index], " Completed: ", completedAndTokenForwarded[index], " State changed: ", stateChanged[index], "Same: ", same[index])
+		// One and only one of the following conditions must be 1 (true): readyInTheRightTime, completedAndTokenForwarded, stateChanged, same
+		readyOrStateChange := api.Xor(readyInTheRightTime[index], api.And(stateChanged[index], utils.Not(api, gotReady)))
+		complatedOrSame := api.Xor(completedAndTokenForwarded[index], same[index])
+		api.Println("Task: ", task.ID, " Final: ", api.Xor(readyOrStateChange, complatedOrSame))
+		api.AssertIsEqual(api.Xor(readyOrStateChange, complatedOrSame), common.TRUE)
+	}
+}
+
+func stage1(N int, circuit Circuit, api frontend.API) ([]frontend.Variable, []frontend.Variable, []frontend.Variable) {
+	same := make([]frontend.Variable, N)
+	complated := make([]frontend.Variable, N)
+	stateChanged := make([]frontend.Variable, N)
+	for i, curr := range circuit.State_curr.States {
+		new := circuit.State_new.States[i]
+		same[i] = utils.IsEqual(api, curr, new)
+		complated[i] = api.Select(same[i], common.FALSE, utils.IsEqual(api, new, common.STATE_COMPLETED))
+
+		lifecycleCheck := lifecycle.LifecycleCheck(api, curr, new)
+		api.AssertIsEqual(api.Select(same[i], common.TRUE, lifecycleCheck), common.TRUE)
+
+		stateChanged[i] = api.Select(same[i], common.FALSE, api.Select(complated[i], common.FALSE, common.TRUE))
+	}
+
+	comp_sum := make([]frontend.Variable, N)
+	diff_sum := make([]frontend.Variable, N)
+
+	comp_sum[0] = complated[0]
+	diff_sum[0] = utils.Not(api, same[0])
+	for i := 1; i < N; i++ {
+		comp_sum[i] = api.Add(comp_sum[i-1], complated[i])
+		diff_sum[i] = api.Add(diff_sum[i-1], utils.Not(api, same[i]))
+	}
+	api.Println("comp_sum: ", comp_sum[N-1])
+	api.Println("diff_sum: ", diff_sum[N-1])
+	noChange := utils.IsEqual(api, diff_sum[N-1], 0)
+	api.Println("No change: ", noChange)
+	isActivated := utils.IsEqual(api, comp_sum[N-1], 1)
+	api.Println("Is activated: ", isActivated)
+	justOne := api.Select(isActivated, 0, utils.IsEqual(api, diff_sum[N-1], 1))
+	api.Println("Just one: ", justOne)
+	validChanges := api.Select(noChange, common.TRUE, api.Xor(isActivated, justOne))
+
+	api.AssertIsEqual(validChanges, common.TRUE)
+	return same, complated, stateChanged
 }
 
 func compressedState(api frontend.API, state State) []frontend.Variable {
